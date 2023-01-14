20ed8fd7da93d3a0dc2ee76d921301d2fb354c25
Hail2DaCode <iovinob2@gmail.com>
2023-01-14 08:59:40 -0700

    SENSEI BONUT

--- userswithBankAccounts.py
+++ userswithBankAccounts.py
@@ -2,16 +2,34 @@ class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account = BankAccount(int_rate=0.02, balance=0)
+        self.account = {"checking": BankAccount(int_rate=0.02, balance=0)}
     
     # other methods
-    
-    def make_deposit(self, amount, repeat = 1):
-    	self.account.deposit(amount, repeat)
-    def make_withdrawal(self, amount, repeat = 1):
-        self.account.withdraw(amount, repeat)
-    def display_user_balance(self):
-        self.account.display_account_info()
+    def add_savings_account(self, int_rate = 0.5, balance = 500):
+        self.account["savings"] = BankAccount(int_rate, balance)
+    def make_deposit(self, key, amount, repeat = 1,):
+        if key == "checking":
+    	    self.account["checking"].deposit(amount, repeat)
+        elif key == "savings":
+            self.account["savings"].deposit(amount, repeat)
+        else:
+            print("Please type in the correct account")
+    def make_withdrawal(self, key, amount, repeat = 1):
+        if key == "checking":
+            self.account["checking"].withdraw(amount,repeat)
+        elif key == "savings":
+            self.account["savings"].withdraw(amount, repeat)
+        else:
+            print("Please type in the correct account")
+    def display_user_balance(self, key = "all"):
+        if key == "all":
+            for account in self.account:
+                print(account, end=": ")
+                self.account[account].display_account_info()
+        elif key == "checking":
+            self.account["checking"].display_account_info()
+        elif key == "savings":
+            self.account["savings"].display_account_info()
 
 
 class BankAccount:
@@ -42,6 +60,7 @@ class BankAccount:
             account.display_account_info()
 
 user_1 = User("Brian", "iovinob2@gmail.com")
+'''
 print(user_1.name)
 user_1.account.display_account_info()
 print(user_1.account.int_rate)
@@ -49,4 +68,11 @@ user_1.make_deposit(1500)
 user_1.account.display_account_info()
 user_1.make_withdrawal(100, 10)
 user_1.account.display_account_info()
+user_1.display_user_balance()'''
+user_1.make_deposit("checking", 1500, 2)
+print(user_1.account["checking"].balance)
+user_1.display_user_balance()
+user_1.add_savings_account()
+user_1.display_user_balance()
+user_1.make_withdrawal("checking", 500, 2)
 user_1.display_user_balance()
